69c34171962a502015b6dd1cdf414de8d61e2b9f
Enguerran <engcolson@gmail.com>
2016-05-03 18:22:41 +0200

    manage the battle components

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,13 +1,44 @@
 var React = require('react');
-
-function puke(obj) {
-  return <pre>{JSON.stringify(obj, null, ' ')}</pre>
-}
+var PropTypes = React.PropTypes;
+var styles = require('../styles');
+var Link = require('react-router').Link;
+var UserDetails = require('./UserDetails');
+var UserDetailsWrapper = require('./UserDetailsWrapper');
 
 function ConfirmBattle(props) {
   return props.isLoading
     ? <p> Loading </p>
-    : <p> Confirm Battle: {puke(props.playersInfo)} </p>
+    : <div className="jumbotro, col-sm-12 text-center" style={styles.transparentBg}>
+        <h1>Confirm Players</h1>
+        <div className="col-sm-8 col-sm-offset-2">
+          <UserDetailsWrapper header={'Player One'}>
+            <UserDetails info={props.playersInfo[0]} />
+          </UserDetailsWrapper>
+          <UserDetailsWrapper header={'Player Two'}>
+            <UserDetails info={props.playersInfo[1]} />
+          </UserDetailsWrapper>
+        </div>
+        <div className="col-sm-8 col-sm-offset-2">
+          <div className="col-sm-12" style={styles.space}>
+            <button type="button" className="btn btn-lng btn-success" onClick={props.onInitiateBattle}>
+              Initiate Battle
+            </button>
+          </div>
+          <div className="col-sm-12" style={styles.space}>
+            <Link to='/playerOne'>
+              <button className="btn btn-lng btn-warning">
+                Reselect Players
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+}
+
+ConfirmBattle.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
+  onInitiateBattle: PropTypes.func.isRequired,
+  playersInfo: PropTypes.array.isRequired
 }
 
 module.exports = ConfirmBattle;
--- app/utils/GithubHelpers.js
+++ app/utils/GithubHelpers.js
@@ -2,17 +2,49 @@ var id = "YOUR_CLIENT_ID";
 var sec = "YOUR_SECRET_ID";
 var param = "?client_id=" + id + "&client_secret=" + sec;
 
-function getUserInfo(username) {
+function getUserInfo (username) {
   return fetch('https://api.github.com/users/' + username + param).then(function(response) {
     return response.json();
   });
 }
 
+function getRepos (username) {
+  return fetch('https://api.github.com/users/' + username + '/repos' + param + '&per_page=100').then(function(response) { return response.json(); });
+}
+
+function getTotalStars (repos) {
+  return repos.reduce(function(prev, current) {
+    return prev + current.stargazers_count
+  }, 0);
+}
+
+function getPlayersData (player) {
+  return getRepos(player.login)
+  .then(getTotalStars)
+  .then(function(totalStars) {
+    return {
+      followers: player.followers,
+      totalStars: totalStars
+    }
+  });
+}
+
+function calculateScore (players) {
+  return players.map(function(player) {
+    return player.followers * 3 + player.totalStars;
+  });
+}
+
 var helpers = {
-  getPlayersInfo: function(players) {
+  getPlayersInfo: function (players) {
     return Promise.all(players.map(getUserInfo)).catch(function(err) {
       console.warn('Error in getPlayersInfo', err);
     });
+  },
+  battle: function (players) {
+    return Promise.all(players.map(getPlayersData))
+      .then(calculateScore)
+      .catch(function(err) { console.warn('Error in getPlayersData: ', err)});
   }
 }
 
