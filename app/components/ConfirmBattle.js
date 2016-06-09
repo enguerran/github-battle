c037ff72448d6f2692ace4d7da55c219c169b42f
Enguerran <engcolson@gmail.com>
2016-06-09 17:44:56 +0200

    add container and loading

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -4,11 +4,13 @@ var styles = require('../styles');
 var Link = require('react-router').Link;
 var UserDetails = require('./UserDetails');
 var UserDetailsWrapper = require('./UserDetailsWrapper');
+var MainContainer = require('./MainContainer');
+var Loading = require('./Loading');
 
 function ConfirmBattle(props) {
   return props.isLoading
-    ? (<p> Loading </p>)
-    : (<div className="jumbotro, col-sm-12 text-center" style={styles.transparentBg}>
+    ? (<Loading speed={800} text='waiting' />)
+    : (<MainContainer>
         <h1>Confirm Players</h1>
         <div className="col-sm-8 col-sm-offset-2">
           <UserDetailsWrapper header={'Player One'}>
@@ -32,7 +34,7 @@ function ConfirmBattle(props) {
             </Link>
           </div>
         </div>
-      </div>);
+      </MainContainer>);
 }
 
 ConfirmBattle.propTypes = {
